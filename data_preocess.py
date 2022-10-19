58fb327b8a5cba84caf10a264e2b6452c23e7c78
sleekEagle <lahirunuwanisme@gmail.com>
2022-10-19 14:53:30 -0400

    data preprocess

--- data_preocess.py
+++ data_preocess.py
@@ -4,7 +4,8 @@ from matplotlib import pyplot as plt
 import camera_calibration.calibdata as calib
 import numpy as np
 from scipy import interpolate
-import math
+import cv2
+import os
 
 '''
 We need to interpolate the depth image after coordinate frame transformation because the 
@@ -40,70 +41,122 @@ def interpolate_missing_pixels(
 
     return interp_image
 
+def save_transformed_depth_image(ptcfile,phonecalibfile,Rpath,Tpath,depthpath):
 
-x,y,z=imgread.read_ptc(r'C:\Users\lahir\fstack_data\data\10_18_2022_16_20_44\000.ptc')
+    x,y,z=imgread.read_ptc(ptcfile)
 
-#read the calibration matrix of phone
-phone_intri=calib.read_mtx_from_csv(r'C:\Users\lahir\fstack_data\calibration\phone_calib.csv',3)
-phone_intri=calib.get_3x4intr(phone_intri)
-#read the translation and rotation matrices kinect -> phone
-R=calib.read_mtx_from_csv(r'C:\Users\lahir\fstack_data\calibration\azuretophone_R.csv',3)
-T=calib.read_mtx_from_csv(r'C:\Users\lahir\fstack_data\calibration\azuretophone_T.csv',1)
-RT=calib.get_transmtx_from_RT(R,T)
+    #read the calibration matrix of phone
+    phone_intri=calib.read_mtx_from_csv(phonecalibfile,3)
+    phone_intri=calib.get_3x4intr(phone_intri)
+    #read the translation and rotation matrices kinect -> phone
+    R=calib.read_mtx_from_csv(Rpath,3)
+    T=calib.read_mtx_from_csv(Tpath,1)
+    RT=calib.get_transmtx_from_RT(R,T)
 
-trans_pts=[]
-for i in range(len(x)):
-    pt=np.array([[list(x[i])[0],list(y[i])[0],list(z[i])[0]]])
-    pt=calib.get_homo(pt).T
-    #transform point to phone coordinate system
-    pt_trans=np.matmul(RT,pt)
-    pt_trans=pt_trans/pt_trans[-1]
-    #get distance to the point from camera center
-    dist=list(np.sqrt(pt_trans[0]*pt_trans[0] + pt_trans[1]*pt_trans[1] + pt_trans[2]*pt_trans[2]))[0]
-    #transform this point to image coordinate system with phone's intrinsic matrix
-    pt_trans=np.matmul(phone_intri,pt_trans)
-    pt_trans=list(pt_trans[:,0])
-    
-    if(pt_trans[-1]==0):
-        trans_pts.append([0.0,0.0,0.0])
-    else:
-        trans_pts.append([pt_trans[0]/pt_trans[-1],pt_trans[1]/pt_trans[-1],dist])
+    trans_pts=[]
+    for i in range(len(x)):
+        pt=np.array([[list(x[i])[0],list(y[i])[0],list(z[i])[0]]])
+        pt=calib.get_homo(pt).T
+        #transform point to phone coordinate system
+        pt_trans=np.matmul(RT,pt)
+        pt_trans=pt_trans/pt_trans[-1]
+        #get distance to the point from camera center
+        dist=list(np.sqrt(pt_trans[0]*pt_trans[0] + pt_trans[1]*pt_trans[1] + pt_trans[2]*pt_trans[2]))[0]
+        #transform this point to image coordinate system with phone's intrinsic matrix
+        pt_trans=np.matmul(phone_intri,pt_trans)
+        pt_trans=list(pt_trans[:,0])
+        
+        if(pt_trans[-1]==0):
+            trans_pts.append([0.0,0.0,0.0])
+        else:
+            trans_pts.append([pt_trans[0]/pt_trans[-1],pt_trans[1]/pt_trans[-1],dist])
 
-#taking round should be more accurate than using the floor and ceil method below (commented)
-image=np.zeros(imgread.transformed_shape)
-for pt in trans_pts:
-    if(round(pt[0])>0 and round(pt[0])<imgread.transformed_shape[1] and round(pt[1])>0 and round(pt[1])<imgread.transformed_shape[0]):
-        image[round(pt[1]),round(pt[0])]=pt[-1]
+    #taking round should be more accurate than using the floor and ceil method below (commented)
+    image=np.zeros(imgread.transformed_shape)
+    for pt in trans_pts:
+        if(round(pt[0])>0 and round(pt[0])<imgread.transformed_shape[1] and round(pt[1])>0 and round(pt[1])<imgread.transformed_shape[0]):
+            image[round(pt[1]),round(pt[0])]=pt[-1]
+    '''
+    image=np.zeros(imgread.transformed_shape)
+    for pt in trans_pts:
+        if(round(pt[0])>0 and math.ceil(pt[0])<imgread.transformed_shape[1] and round(pt[1])>0 and math.ceil(pt[1])<imgread.transformed_shape[0]):
+            image[math.ceil(pt[1]),math.ceil(pt[0])]=pt[-1]
+            image[math.floor(pt[1]),math.floor(pt[0])]=pt[-1]
+    '''
+
+    #mask = what pixels are needed of interpolating ?
+    #criteria to select these pixels : if at least two pixles of the surrounding pixels are not zero
+    #because the artefact zero pixels are mostly single pixles. Pixels around them have proper values.
+    mask=np.zeros_like(image)
+    for h in range(1,image.shape[0]-1):
+        for w in range(1,image.shape[1]-1):
+            n_nonzero=0
+            if(image[h,w]==0):
+                #look around this pixel
+                if(image[h-1,w]>0): n_nonzero+=1
+                if(image[h+1,w]>0): n_nonzero+=1
+                if(image[h,w-1]>0): n_nonzero+=1
+                if(image[h,w+1]>0): n_nonzero+=1
+                if(image[h-1,w-1]>0): n_nonzero+=1
+                if(image[h-1,w+1]>0): n_nonzero+=1
+                if(image[h+1,w-1]>0): n_nonzero+=1
+                if(image[h+1,w+1]>0): n_nonzero+=1
+                if(n_nonzero>=2):
+                    mask[h,w]=1
+    mask=mask==1
+
+    intimg=interpolate_missing_pixels(image,mask)
+    res=cv2.imwrite(depthpath, intimg.astype(np.uint16))
+    return res
+
+
 '''
-image=np.zeros(imgread.transformed_shape)
-for pt in trans_pts:
-    if(round(pt[0])>0 and math.ceil(pt[0])<imgread.transformed_shape[1] and round(pt[1])>0 and math.ceil(pt[1])<imgread.transformed_shape[0]):
-        image[math.ceil(pt[1]),math.ceil(pt[0])]=pt[-1]
-        image[math.floor(pt[1]),math.floor(pt[0])]=pt[-1]
+selects all focal stack images (all .jpg files in path are considered focal stack images).
+If there are multiple images with the same focal distance, only selects one of them.
+coppies them to newpath/dirname
+dirname is the name of the directory in the path where focal stack images are.
 '''
+def copy_fs_files_to_dir(path,newpath):
+    files=os.listdir(path)
+    #get only one instance from one focal distance
+    fstack_files=[f for f in files if (f.split('_')[-1].split('.')[-1]=='jpg')]
+    fdists=[]
+    ufiles=[]
+    for f in fstack_files:
+        fd=f.split('_')[-1][:-4]
+        if(not (fd in fdists)):
+            fdists.append(fd)
+            ufiles.append(f)
 
-#mask = what pixels are needed of interpolating ?
-#criteria to select these pixels : if at least two pixles of the surrounding pixels are not zero
-#because the artefact zero pixels are mostly single pixles. Pixels around them have proper values.
-mask=np.zeros_like(image)
-for h in range(1,image.shape[0]-1):
-    for w in range(1,image.shape[1]-1):
-        n_nonzero=0
-        if(image[h,w]==0):
-            #look around this pixel
-            if(image[h-1,w]>0): n_nonzero+=1
-            if(image[h+1,w]>0): n_nonzero+=1
-            if(image[h,w-1]>0): n_nonzero+=1
-            if(image[h,w+1]>0): n_nonzero+=1
-            if(image[h-1,w-1]>0): n_nonzero+=1
-            if(image[h-1,w+1]>0): n_nonzero+=1
-            if(image[h+1,w-1]>0): n_nonzero+=1
-            if(image[h+1,w+1]>0): n_nonzero+=1
-            if(n_nonzero>=2):
-                mask[h,w]=1
-mask=mask==1
+    for f in ufiles:
+        img = cv2.imread(path+'\\'+f)
+        res=cv2.imwrite(newpath+'\\'+dirtomake+'\\'+f,img)
 
-res=interpolate_missing_pixels(image,mask)
-plt.imshow(res)
-plt.show()
+
+
+ptcfile=r'C:\Users\lahir\fstack_data\data\10_18_2022_16_20_44\000.ptc'
+phonecalibfile=r'C:\Users\lahir\fstack_data\calibration\phone_calib.csv'
+Rpath=r'C:\Users\lahir\fstack_data\calibration\azuretophone_R.csv'
+Tpath=r'C:\Users\lahir\fstack_data\calibration\azuretophone_T.csv'
+newpath=r'C:\Users\lahir\fstack_data\data_processed'
+path=r'C:\Users\lahir\fstack_data\data\10_18_2022_16_20_44'
+
+#make new directory with to contain the focal stack and depth image
+dirtomake=path.split('\\')[-1]
+if not os.path.exists(newpath+'\\'+dirtomake):
+    os.makedirs(newpath+'\\'+dirtomake)
+depthpath=newpath+'\\'+dirtomake+'\\'+'depth.png'
+#save depth image
+save_transformed_depth_image(ptcfile,phonecalibfile,Rpath,Tpath,depthpath)
+#copy focal stack 
+copy_fs_files_to_dir(path,newpath)
+
+
+
+
+
+
+
+
+
 
